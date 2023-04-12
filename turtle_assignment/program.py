f33ca55a213c27e2aa7fb55ac1918328fee96771
SuperHarmony910 <57850338+SuperHarmony910@users.noreply.github.com>
2023-04-12 13:41:54 +1000

    created checkers board

--- turtle_assignment/program.py
+++ turtle_assignment/program.py
@@ -1,11 +1,46 @@
 from turtle import *
+i = 8
+global c = 0
+#speed(999999)
+penup()
+left(90)
+forward(200)
+left(90)
+forward(200)
+left(180)
+pendown()
+def create_square():
+    c = self.c
+    if c == 0:
+        fillcolor("saddlebrown")
+        c = 1
+    else:
+        fillcolor("#efceac")
+        c = 0
+    begin_fill()
+    forward(60)
+    right(90)
+    forward(60)
+    right(90)
+    forward(60)
+    right(90)
+    forward(60)
+    right(90)
+    forward(60)
+    end_fill()
 
-for i in range(7):
-    left(60)
-    forward(100)
-    right(120)
-    forward(100)
-    left(60)
-    forward(100)
-    left(180)
-    forward(200)
+while i > 1:
+    for j in range(i):
+        create_square()
+    right(90)
+    for j in range(i):
+        create_square()
+    right(90)
+    for j in range(i):
+        create_square()
+    right(90)
+    for j in range(i):
+        create_square()
+    right(90)
+    i -= 1
+    print(i)
