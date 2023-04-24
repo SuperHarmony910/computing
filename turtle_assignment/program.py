c087fba6ef67c227d5fd8135ca276739f9599812
SuperHarmony910 <57850338+SuperHarmony910@users.noreply.github.com>
2023-04-24 19:31:41 +1000

    the checker pieces are now complete!

--- turtle_assignment/program.py
+++ turtle_assignment/program.py
@@ -1,9 +1,23 @@
 from turtle import *
-i = 7
-c = 0
+
+# setting
+setup(800, 800)
+window = Screen()
+window.title("Checkers Assignment")
+window.bgcolor("darkslateblue")
+
+i = 7  # square iterator
+c = 0  # colour iterator
+board = []  # board array
+
+for i in range(8):
+    board.append([])
+    for j in range(8):
+        board[i].append(0)
 
 # quick speed for faster debugging
-speed(999999)
+#speed(0)
+tracer(0, 0)
 
 # position to top left corner
 penup()
@@ -13,6 +27,10 @@ left(90)
 forward(200)
 left(180)
 pendown()
+
+# function to create a square
+
+
 def create_square(endfill=0):
     global c
     if endfill == 1:
@@ -38,6 +56,35 @@ def create_square(endfill=0):
     forward(60)
     end_fill()
 
+# create circle from top right corner of square
+
+
+def create_circle(colour):
+    if colour == "white":
+        fillcolor("#e9ce9b")
+        pencolor("#d2b26d")
+    else:
+        fillcolor("#000000")
+        pencolor("#8c510a")
+    forward(5)
+    right(90)
+    forward(30)
+    pendown()
+    begin_fill()
+    circle(25)
+    penup()
+    left(90)
+    forward(5)
+    right(90)
+    pendown()
+    circle(20)
+    end_fill()
+    penup()
+    back(30)
+    left(90)
+    back(10)
+
+
 # loop to create board with squares of alternating colours
 while i > 1:
     for j in range(i):
@@ -58,6 +105,7 @@ while i > 1:
     forward(60)
     i -= 1
     print(i)
+
 # create final square
 create_square(1)
 
@@ -67,8 +115,49 @@ right(90)
 forward(120)
 right(90)
 forward(120)
-left(90)
-pendown()
+right(90)
+# create_circle("white")
 
+# use board array to put pieces in correct positions
+for y in range(8):
+    for x in range(8):
+        if (y + x) % 2 != 0:
+            forward(60*x)
+            right(90)
+            forward(60*y)
+            left(90)
+            if y < 3:
+                create_circle("white")
+            elif y > 4:
+                create_circle("black")
+            # return to original pos
+            back(60*x)
+            right(90)
+            back(60*y)
+            left(90)
 
 input("Press enter to exit...")
+
+
+# 0 [[0, 1, 0, 3, 0, 5, 0, 7],
+#1 [0, 0, 2, 0, 4, 0, 6, 0],
+#2 [0, 0, 0, 0, 0, 0, 0, 0],
+#3 [0, 0, 0, 0, 0, 0, 0, 0],
+#4 [0, 0, 0, 0, 0, 0, 0, 0],
+#5 [0, 0, 0, 0, 0, 0, 0, 0],
+#6 [0, 0, 0, 0, 0, 0, 0, 0],
+# 7 [0, 0, 0, 0, 0, 0, 0, 0]]
+
+
+# 0, 1 true
+# 0, 3 true
+# 0, 5 true
+# 0, 7 true
+# 1, 0 true
+# 1, 2 true
+# 1, 4 true
+# 1, 6 true
+# 2, 1 true
+# 2, 3 true
+# 2, 5 true
+# 2, 7 true
