0306e491db2e7a55b2cb5d6ac7728579e0d516f8
SuperHarmony910 <57850338+SuperHarmony910@users.noreply.github.com>
2023-04-30 00:44:48 +1000

    creating key-value pairs that correspond to checker pieces (see comment) https://bobbyhadz.com/blog/python-keyerror-0 - for some silly reason, you can't just immediately set a key-value pair apprently? and you have to initialise the key before you give it a value. anyway this has a quick fix but i have no internet anymore and i had the google page open so rather unfortunate so future me just solve this thankss

--- turtle_assignment/program.py
+++ turtle_assignment/program.py
@@ -2,7 +2,7 @@ from enum import Enum
 from turtle import *
 
 # setting
-setup(600, 600)
+setup(0.5401041667, 0.80546875, 0.5, 0.5)
 window = Screen()
 window.title("Checkers Assignment")
 window.bgcolor("darkslateblue")
@@ -17,8 +17,8 @@ for i in range(8):
         board[i].append(0)
 
 # quick speed for faster debugging
-speed(0) # 0 = fastest, 1 = slowest - adjust before submission!
-tracer(0, 0) # instant drawing (no animation), very useful for debugging
+speed(0)  # 0 = fastest, 1 = slowest - adjust before submission!
+tracer(0, 0)  # instant drawing (no animation), very useful for debugging
 
 # position to top left corner
 penup()
@@ -30,7 +30,25 @@ left(180)
 pendown()
 
 
+# create an enum for the pieces
+class PieceType(Enum):
+    EMPTY = 0
+    WHITE = 1
+    BLACK = 2
+    WHITE_KING = 3
+    BLACK_KING = 4
+    WHITE_SELECTED = 5
+    BLACK_SELECTED = 6
+    WHITE_KING_SELECTED = 7
+    BLACK_KING_SELECTED = 8
+    WHITE_MOVE = 9
+    BLACK_MOVE = 10
+    WHITE_KING_MOVE = 11
+    BLACK_KING_MOVE = 12
+
 # function to create a square
+
+
 def create_square(endfill=0):
     global c
     if endfill == 1:
@@ -57,14 +75,16 @@ def create_square(endfill=0):
     end_fill()
 
 
-# function to create a circle from top right corner of square
-def create_circle(type):
+checker_pieces = {}  # y, x, type
+# ^^ think about renaming var to 'board' when time permits after commit (its late and im tired)
+# function to create a checker piece from top right corner of square
+
+
+def create_piece(type):
     if type == "white":
-        fillcolor("#e9ce9b")
-        pencolor("#d2b26d")
+        color("#d2b26d", "#e9ce9b")
     else:
-        fillcolor("#300c09")
-        pencolor("#1b100b")
+        color("#1b100b", "#300c09")
     pensize(3)
     forward(5)
     right(90)
@@ -104,7 +124,7 @@ while i > 1:
     right(90)
     forward(60)
     i -= 1
-    print(i)
+    # print(i)
 
 # create final square
 create_square(1)
@@ -126,9 +146,12 @@ for y in range(8):
             forward(60*y)
             left(90)
             if y < 3:
-                create_circle("white")
+                checker_pieces[int(round(pos()[1]-200)/-60)][int(round(pos()[0]+260)/60)] = pos()[1]/60, pos()[0]/60, "white"
+                create_piece("white")
             elif y > 4:
-                create_circle("black")
+                checker_pieces[int(round(pos()[1]-200)/-60)][int(round(pos()[0]+260)/60)] = pos()[1]/60, pos()[0]/60, "black"
+                create_piece("black")
+
             # return to original pos
             back(60*x)
             right(90)
@@ -141,15 +164,21 @@ for y in range(8):
     for x in range(8):
         forward(60*x)
         right(90)
-        forward(60*y)
+        forward(60*y+30)
         left(90)
-        stamp()
-        write(str(x) + ", " + str(y), align="left")
+        forward(30)
+        color('deepskyblue')
+        write(str(x) + ", " + str(y), align="center",
+              font=("Arial", 18, "normal"))
+        color('black')
+        back(30)
         back(60*x)
         right(90)
-        back(60*y)
+        back(60*y+30)
         left(90)
 
+# log checker pieces dict
+print(checker_pieces)
 
 # make this checkers board playable
 # 0 = empty, 1 = white, 2 = black
@@ -160,6 +189,8 @@ for y in range(8):
 # 11 = white king move, 12 = black king move
 
 # create a class for the pieces
+
+
 class Piece:
     def __init__(self, x, y, colour):
         self.x = x
@@ -168,65 +199,97 @@ class Piece:
         self.king = False
         self.selected = False
 
-# create an enum for the pieces
-class PieceType(Enum):
-    EMPTY = 0
-    WHITE = 1
-    BLACK = 2
-    WHITE_KING = 3
-    BLACK_KING = 4
-    WHITE_SELECTED = 5
-    BLACK_SELECTED = 6
-    WHITE_KING_SELECTED = 7
-    BLACK_KING_SELECTED = 8
-    WHITE_MOVE = 9
-    BLACK_MOVE = 10
-    WHITE_KING_MOVE = 11
-    BLACK_KING_MOVE = 12
+# select a piece to move
 
-# move the animated checker piece to the selected square
+
+def select_piece(x, y):
+    match board[y][x]:
+        # check if the selected piece is a white piece
+        case PieceType.WHITE:
+            if board[y][x] == PieceType.WHITE_SELECTED:
+                return PieceType.EMPTY
+            else:
+                return PieceType.WHITE_SELECTED
+        # check if the selected piece is a black piece
+        case PieceType.BLACK:
+            if board[y][x] == PieceType.BLACK_SELECTED:
+                return PieceType.EMPTY
+            else:
+                return PieceType.BLACK_SELECTED
+        # check if the selected piece is a white king
+        case PieceType.WHITE_KING:
+            if board[y][x] == PieceType.WHITE_KING_SELECTED:
+                return PieceType.EMPTY
+            else:
+                return PieceType.WHITE_KING_SELECTED
+        # check if the selected piece is a black king
+        case PieceType.BLACK_KING:
+            if board[y][x] == PieceType.BLACK_KING_SELECTED:
+                return PieceType.EMPTY
+            else:
+                return PieceType.BLACK_KING_SELECTED
+        # check if the selected piece is nonexistent
+        case _:
+            return PieceType.EMPTY
+
+
+# create the animation for the checker piece to move
 def move_piece(x, y, x2, y2):
-    # move the piece to the selected square
-    forward(60*x2)
-    right(90)
-    forward(60*y2)
-    left(90)
-    # draw the piece
-    if board[y2][x2] == PieceType.WHITE:
-        create_circle("white")
-    elif board[y2][x2] == PieceType.BLACK:
-        create_circle("black")
-    elif board[y2][x2] == PieceType.WHITE_KING:
-        create_circle("white")
-    elif board[y2][x2] == PieceType.BLACK_KING:
-        create_circle("black")
-    # return to original pos
-    back(60*x2)
-    right(90)
-    back(60*y2)
-    left(90)
+    # check if the move is valid
+    if check_move(x, y, x2, y2):
+        # move the piece
+        board[y2][x2] = board[y][x]
+        board[y][x] = PieceType.EMPTY
+        # check if the piece is a king
+        if board[y2][x2] == PieceType.WHITE and y2 == 7:
+            board[y2][x2] = PieceType.WHITE_KING
+        if board[y2][x2] == PieceType.BLACK and y2 == 0:
+            board[y2][x2] = PieceType.BLACK_KING
+        # check if the move is a jump
+        match abs(x - x2):
+            case 2:
+                board[(y + y2) // 2][(x + x2) // 2] = PieceType.EMPTY
+                return 'jump'
+            # check if the move is a double jump
+            case 4:
+                board[(y + y2) // 2][(x + x2) // 2] = PieceType.EMPTY
+                board[(y + y2) // 2][(x + x2) // 2] = PieceType.EMPTY
+                return 'double jump'
+            # check if the move is a triple jump
+            case 6:
+                board[(y + y2) // 2][(x + x2) // 2] = PieceType.EMPTY
+                board[(y + y2) // 2][(x + x2) // 2] = PieceType.EMPTY
+                board[(y + y2) // 2][(x + x2) // 2] = PieceType.EMPTY
+                return 'triple jump'
+
 
 # function to check if a move is valid
 def check_move(x, y, x2, y2):
     # check if move is diagonal
     if abs(x - x2) != abs(y - y2):
         return False
+
     # check if move is 1 square
     if abs(x - x2) != 1:
         return False
+
     # check if move is in bounds
     if x2 < 0 or x2 > 7 or y2 < 0 or y2 > 7:
         return False
+
     # check if move is empty
     if board[y2][x2] != 0:
         return False
+
     # check if move is in correct direction
     if board[y][x] == 1 and y2 > y:
         return False
+
     if board[y][x] == 2 and y2 < y:
         return False
+
     # check if move is a jump
-    if abs(x - x2) == 2:
+    if abs(x - x2) % 2 == 0:  # accounts for all jump magnitudes
         if board[y][x] == 1:
             if board[y2 + 1][x2 + 1] != 2 and board[y2 + 1][x2 - 1] != 2:
                 return False
@@ -234,7 +297,6 @@ def check_move(x, y, x2, y2):
             if board[y2 - 1][x2 + 1] != 1 and board[y2 - 1][x2 - 1] != 1:
                 return False
     return True
-
 
 
 # function to check if a jump is valid
@@ -259,31 +321,37 @@ def check_jump(x, y, x2, y2):
     # check if jump is over an enemy piece
     if board[y][x] == 1:
         if x2 > x and board[y + 1][x + 1] != 2:
-                return False
+            return False
         if x2 < x and board[y + 1][x - 1] != 2:
-                return False
+            return False
     if board[y][x] == 2:
         if x2 > x and board[y - 1][x + 1] != 1:
-                return False
+            return False
         if x2 < x and board[y - 1][x - 1] != 1:
-                return False
+            return False
     return True
 
 # function to check if a piece can jump
+
+
 def check_jump_available(x, y):
-    if board[y][x] == 1:
-        if x > 1 and check_jump(x, y, x - 2, y + 2):
+    match board[y][x]:
+        case 1:
+            if x > 1 and check_jump(x, y, x - 2, y + 2):
                 return True
-        if x < 6 and check_jump(x, y, x + 2, y + 2):
+            if x < 6 and check_jump(x, y, x + 2, y + 2):
                 return True
-    if board[y][x] == 2:
-        if x > 1 and check_jump(x, y, x - 2, y - 2):
+        case 2:
+            if x > 1 and check_jump(x, y, x - 2, y - 2):
                 return True
-        if x < 6 and check_jump(x, y, x + 2, y - 2):
+            if x < 6 and check_jump(x, y, x + 2, y - 2):
                 return True
-    return False
+        case _:
+            return False
 
 # make piece jump
+
+
 def jump(x, y, x2, y2):
     board[y2][x2] = board[y][x]
     board[y][x] = 0
@@ -305,17 +373,19 @@ def jump(x, y, x2, y2):
             if x2 < x:
                 board[y2 + 1][x2 + 1] = PieceType.EMPTY
 
+goto(-200, 200)
+stamp()
 input("Press enter to exit...")
 
 
-#0 [[0, 1, 0, 3, 0, 5, 0, 7],
+# 0 [[0, 1, 0, 3, 0, 5, 0, 7],
 #1 [0, 0, 2, 0, 4, 0, 6, 0],
 #2 [0, 0, 0, 0, 0, 0, 0, 0],
 #3 [0, 0, 0, 0, 0, 0, 0, 0],
 #4 [0, 0, 0, 0, 0, 0, 0, 0],
 #5 [0, 0, 0, 0, 0, 0, 0, 0],
 #6 [0, 0, 0, 0, 0, 0, 0, 0],
-#7 [0, 0, 0, 0, 0, 0, 0, 0]]
+# 7 [0, 0, 0, 0, 0, 0, 0, 0]]
 
 
 # 0, 1 true
